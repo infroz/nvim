2a4313752fb924b8631244c7c90eb6c0d4428e18
Vebjørn Leiros <infroz98@gmail.com>
2025-10-31 10:56:29 +0100

    Enhance install.sh for Neovim installation
    Refactor installation script to improve directory handling and add checks for existing directories. Update commands for installing Neovim and its dependencies.

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+  GNU nano 7.2                                                                                                             install.sh
 #!/bin/bash
 # Script to update and install the latest Neovim from the nightly branch
 
@@ -6,11 +7,34 @@ set -e
 
 # Navigate to the neovim source directory
 # update: adjust path to install in config location
-mkdir -p ~/config/nvim/neovim.git/
-cd ~/config/nvim/neovim.git/
+mkdir -p ~/.config/nvim/
+cd ~/.config/nvim/
 
-echo "--- Pulling latest changes from nightly branch ---"
+## check if folder exists
+if [ ! -d ~/.config/nvim/neovim.git ]; then
+        ## should be in .config/nvim
+        git clone https://github.com/neovim/neovim neovim.git
+fi
+
+cd ~/.config/nvim/neovim.git
+git checkout nightly
 git pull origin nightly
+
+## update apt
+sudo apt-get update
+
+## Install neovim deps
+## https://github.com/neovim/neovim/wiki/Building-Neovim/688be28f98c18e73b5043879b5963287a9b13d6c#ubuntu--debian
+sudo apt-get install ninja-build gettext cmake unzip curl
+
+## Install neovim plugin deps
+sudo apt-get install fzf ripgrep
+
+## install pnpm
+curl -fsSL https://get.pnpm.io/install.sh | sh -
+source ~/.bashrc
+
+pnpm env use --global lts
 
 echo "--- Cleaning up old build artifacts ---"
 # Thoroughly clean the project before building
@@ -26,5 +50,5 @@ echo "--- Installing the new Neovim package ---"
 sudo dpkg -i nvim-linux-x86_64.deb
 
 echo "--- Update Complete! ---"
-echo "Neovim version:"
+echo "Neovim version:
 nvim -v
